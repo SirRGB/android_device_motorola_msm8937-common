e0e92ddb7fb99907194535eef0c54cef137a561b
Jarl-Penguin <jarlpenguin@outlook.com>
2021-08-08 10:38:48 +0300

    msm8937-common: Add odm/product/system_ext partitions
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I47504830278c350455a2a01bd33f78d19c8463bb

--- releasetools.py
+++ releasetools.py
@@ -12,23 +12,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import common
-import re
+import os
+from common import BlockDifference, EmptyImage, GetUserImage
 
-def AddBasebandAssertion(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-baseband\s*=\s*(\S+)', android_info.decode('utf-8'))
-  if m:
-    versions = m.group(1).split('|')
-    if len(versions) and '*' not in versions:
-      cmd = 'assert(motorola.verify_baseband(' + ','.join(['"%s"' % baseband for baseband in versions]) + ') == "1" || abort("ERROR: This package requires firmware from an Android 8.1 based stock ROM build. Please upgrade firmware and retry!"););'
-      info.script.AppendExtra(cmd)
-  return
+# The joined list of user image partitions of source and target builds.
+# - Items should be added to the list if new dynamic partitions are added.
+# - Items should not be removed from the list even if dynamic partitions are
+#   deleted. When generating an incremental OTA package, this script needs to
+#   know that an image is present in source build but not in target build.
+USERIMAGE_PARTITIONS = [
+    "odm",
+    "product",
+    "system_ext",
+]
 
-def FullOTA_Assertions(info):
-  AddBasebandAssertion(info)
-  return
+def GetUserImages(input_tmp, input_zip):
+  return {partition: GetUserImage(partition, input_tmp, input_zip)
+          for partition in USERIMAGE_PARTITIONS
+          if os.path.exists(os.path.join(input_tmp,
+                                         "IMAGES", partition + ".img"))}
 
-def IncrementalOTA_Assertions(info):
-  AddBasebandAssertion(info)
-  return
+def FullOTA_GetBlockDifferences(info):
+  images = GetUserImages(info.input_tmp, info.input_zip)
+  return [BlockDifference(partition, image)
+          for partition, image in images.items()]
+
+def IncrementalOTA_GetBlockDifferences(info):
+  source_images = GetUserImages(info.source_tmp, info.source_zip)
+  target_images = GetUserImages(info.target_tmp, info.target_zip)
+
+  # Use EmptyImage() as a placeholder for partitions that will be deleted.
+  for partition in source_images:
+    target_images.setdefault(partition, EmptyImage())
+
+  # Use source_images.get() because new partitions are not in source_images.
+  return [BlockDifference(partition, target_image, source_images.get(partition))
+          for partition, target_image in target_images.items()]
