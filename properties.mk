1f99ba68dd39929ba40b4901759223a0da5949c5
Midas Chien <midaschieh@google.com>
2022-11-14 09:56:26 -0300

    msm8937-common: properties: don't latch unsignal buffers
    Set debug.sf.latch_unsignaled=1 is used to allow surfaceflinger to
    latch unsignal buffers that can give more time for buffer to render.
    But if the buffer takes longer time to finish, it will block whole
    display pipeline result to jank.
    
    Bug: 157964883
    Test: scrolling, launch

--- properties.mk
+++ properties.mk
@@ -105,7 +105,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_client_composition_cache=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=0 \
-    debug.sf.latch_unsignaled=1 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.recomputecrop=0 \
     dev.pm.dyn_samplingrate=1 \
