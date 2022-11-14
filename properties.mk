d93464167f16b3c5d6dacf87e631effd5e1277d1
OmarAlCoptan <omarcoptan9@gmail.com>
2022-11-14 10:21:49 -0300

    msm8937-common: props: Disable vsync for CPU rendered Apps
    * Choreographer VSync has some performance impact for CPU rendered apps
    as the app has to wait for VSync to start a new frame draw. When the
    frame draw time exceeds one VSync, this has a direct impact on the FPS.
    debug.cpurend.vsync property controls the VSync for CPU rendered apps.
    
    Disable VSync for CPU rendered app, by setting this property to false.

--- properties.mk
+++ properties.mk
@@ -114,7 +114,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.hwc_set_default_colormode=true \
     ro.vendor.display.cabl=0 \
     ro.hardware.egl=adreno \
-    ro.launcher.blur.appLaunch=0
+    ro.launcher.blur.appLaunch=0 \
+    debug.cpurend.vsync=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000 \
