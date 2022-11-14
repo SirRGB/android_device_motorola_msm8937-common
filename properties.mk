b68bfd2e3799df03bd88e4afb92c5d888e851445
rany <ranygh@riseup.net>
2022-11-14 10:12:11 -0300

    msm8937-common: Disable blur on app launch
    This change adds the system property that disables blur during app
    launch.

--- properties.mk
+++ properties.mk
@@ -114,6 +114,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.hwc_set_default_colormode=true \
     ro.vendor.display.cabl=0 \
     ro.hardware.egl=adreno \
+    ro.launcher.blur.appLaunch=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000 \
