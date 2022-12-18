67d7fd2f7e715fb53116ebb432d6cf7f743d9432
SirRGB <sirrgb@proton.me>
2022-12-18 09:11:57 +0100

    msm8937-common: nuke wfd common jar FAILED: out/soong/build.ninja out/soong/.bootstrap/bin/soong_build -t -l out/.module_paths/Android.bp.list -b out/soong -n out -d out/soong/build.ninja.d -globFile out/soong/.bootstrap/build-globs.ninja -o out/soong/build.ninja Android.bp internal error: failed to find dex jar path for module "WfdCommon"

--- msm8937.mk
+++ msm8937.mk
@@ -405,5 +405,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_wlan_nv.bin
 
 # WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+# PRODUCT_BOOT_JARS += \
+#    WfdCommon
