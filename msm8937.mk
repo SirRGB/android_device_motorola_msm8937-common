ebbb2d8fb1a64a1a8a4e24dd13bac4103f31915b
SirRGB <sirrgb@proton.me>
2023-02-04 10:19:24 +0100

    msm8937-common: remove dead targets
    FAILED:
    build/make/core/main.mk:1308: warning:  device/motorola/cedric/lineage_cedric.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    libcnefeatureconfig
    build/make/core/main.mk:1308: error: Build failed.

--- msm8937.mk
+++ msm8937.mk
@@ -80,10 +80,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.5-service
 
-# CNE
-PRODUCT_PACKAGES += \
-    libcnefeatureconfig
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
