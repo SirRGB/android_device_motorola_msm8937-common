1cbcb8eb851019e46e54199604f7741bdc188930
johnmart19 <johnivan19999@gmail.com>
2023-08-07 00:38:59 +0530

    msm8937-common: Enable ro.config.avoid_gfx_accel
    - This flag turns off GPU/CPU intensive UI effects and
      reduces the RAM footprint.
    
    - Using this is a better compromise than the 'low_ram' flag
      as this does not disable features in the rom but still
      reduces RAM usage and gives better smoothness in the UI.
    
    Change-Id: b2a4285551ee79b097f6223c80d14dfb14a67316
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- properties.mk
+++ properties.mk
@@ -113,7 +113,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_skip_validate=1 \
     vendor.display.perf_hint_window=50 \
     vendor.display.disable_rotator_downscale=1 \
-    vendor.gralloc.enable_fb_ubwc=1
+    vendor.gralloc.enable_fb_ubwc=1 \
+    ro.config.avoid_gfx_accel=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000 \
