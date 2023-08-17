d38aeb54813b5bcccb794e02d48d2a22bdf899b3
SirRGB <sirrgb@proton.me>
2023-08-17 17:27:24 +0200

    Revert "msm8937-common: enable hwui render ahead"
    This reverts commit f9074a630c4f92591d6cf6a81eba36890669119e.

--- properties.mk
+++ properties.mk
@@ -139,8 +139,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.supports_background_blur=0 \
     debug.renderengine.backend=threaded \
     debug.sf.predict_hwc_composition_strategy=0 \
-    debug.sf.enable_transaction_tracing=false \
-    ro.hwui.render_ahead=1
+    debug.sf.enable_transaction_tracing=false
 
 # Hardware
 PRODUCT_PROPERTY_OVERRIDES += \
