704d9e68801d10d0309a921a2e55ab6aa55fe953
Edwin Moquete <edwinmmoquete@gmail.com>
2023-12-09 17:10:29 +0000

    msm8937-common: Disable display refresh rate override
    This got enabled by default on U, and it causes apps like
    Chrome and Youtube to set the refresh rate to 30FPS when
    playing some videos.
    
    Change-Id: I649bf03d550c2b9726c7957d15ed09e455d874ec | AOSP
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- properties.mk
+++ properties.mk
@@ -127,7 +127,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.hwc_set_default_colormode=true \
     ro.vendor.display.cabl=0 \
     ro.launcher.blur.appLaunch=0 \
-    debug.cpurend.vsync=false
+    debug.cpurend.vsync=false \
+    ro.surface_flinger.enable_frame_rate_override=false
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
