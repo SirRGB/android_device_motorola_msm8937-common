86bc4f26badfca6f59f82490628559f1acfc5c32
Bruno Martins <bgcngm@gmail.com>
2023-12-21 08:41:17 +0100

    msm8937-common: Drop I/O prefetcher
    This depends on the already removed QTI perf stack.
    
    Change-Id: I06b9a57f868e77bbcbb77c3970aec723404689f4
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- msm8937.mk
+++ msm8937.mk
@@ -283,10 +283,6 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
-# Public Libraries
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson
