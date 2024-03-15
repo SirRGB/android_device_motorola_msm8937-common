b113998a046ace7cf65dbb1c5e4bb4010ef67778
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-15 15:17:25 +0800

    Revert "msm8937-common: Switch to legacy WiFi HIDL HAL"
    * WiFi AIDL HAL works in U QPR2
    
    This reverts commit 7631e8012c5b5e09d28a4ec3d81980434d0b28f8.
    
    Change-Id: I4d38fcc7e5999f934344bc364afb4f4d8a049711

--- msm8937.mk
+++ msm8937.mk
@@ -414,7 +414,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy
+    android.hardware.wifi-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
