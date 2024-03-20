E5Series/device_motorola_msm8937-common-3.18

msm8937-common: Fix screen tearing issue
Set debug.hwui.use_buffer_age=false to prevent
screen tearing on scrolling text. Best option for now.

properties.mk
@@ -129,6 +129,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.avoid_gfx_accel=true \
     ro.vendor.qti.am.reschedule_service=true \
     debug.hwui.renderer=skiagl \
+    debug.hwui.use_buffer_age=false \
     debug.cpurend.vsync=false \
     renderthread.skia.reduceopstasksplitting=true \ 
     debug.renderengine.backend=skiaglthreaded

SirRGB/android_device_motorola_msm8937-common

properties.mk
@@ -115,7 +115,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.cabl=0 \
     ro.hardware.egl=adreno \
     ro.launcher.blur.appLaunch=0 \
-    debug.cpurend.vsync=false
+    debug.cpurend.vsync=false \
+    debug.hwui.use_buffer_age=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000 \