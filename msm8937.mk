10d9b7b4d117dd070953b3149616ca887936ba87
Han Sol Jin <hansol@hansol.ca>
2024-04-26 19:41:02 -0700

    msm8937-common: build common Lineage power-libperfmgr HAL
    * powerhint.json taken from https://github.com/pnplusplus/android_device_xiaomi_Mi8937/commit/9686a4b9bed9610820b1813e8c310cc50e2a348e
    
    Change-Id: I141ba31cff3a8bf6d541ac81cbd570f8b1a8a507
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- msm8937.mk
+++ msm8937.mk
@@ -278,6 +278,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2019-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 endif
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.lineage-libperfmgr
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
