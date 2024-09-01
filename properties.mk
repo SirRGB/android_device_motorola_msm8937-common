7865d7cdfc22397b399ddc76a649dc23ca31133f
SirRGB <sirrgb@proton.me>
2024-09-01 14:29:27 +0200

    Revert "msm8937-common: props: Drop force triple frame buffers"
    This reverts commit d81f502c42eb237c2b54d59fda12bc248caac88d.

--- properties.mk
+++ properties.mk
@@ -102,6 +102,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.use_color_management=true \
     ro.surface_flinger.supports_background_blur=0
